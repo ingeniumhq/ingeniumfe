f75ebf907f15febeb81f6b92ae195cabee030f54
Seyi Abidemi <absdot@yahoo.com>
2024-05-03 16:21:05 +0100

    update certificate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,3 @@
-
-
-
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   devServer: {
@@ -14,47 +11,40 @@ export default defineNuxtConfig({
 
   app: {
     head: {
-      title: 'Ingenium Academy',
+      title: "Ingenium Academy",
       link: [
         {
-          rel: 'icon',
-          type: 'image/x-icon',
-          href: '/images/fav.png',
+          rel: "icon",
+          type: "image/x-icon",
+          href: "/images/fav.png",
         },
       ],
       script: [
         {
           src: "https://code.jquery.com/jquery-3.3.1.slim.min.js",
-          type: "text/javascript"
+          type: "text/javascript",
         },
-       
-      ]
+      ],
     },
   },
-  modules: [
-    '@pinia/nuxt',
-    '@pinia-plugin-persistedstate/nuxt',
-  ],
+  modules: ["@pinia/nuxt", "@pinia-plugin-persistedstate/nuxt", "dayjs-nuxt"],
 
-  vue: {  
+  vue: {
     compilerOptions: {
-      isCustomElement: (tag) => ['vue-lightbox-advanced', 'lightbox'].includes(tag),
+      isCustomElement: (tag) =>
+        ["vue-lightbox-advanced", "lightbox"].includes(tag),
     },
   },
 
   piniaPersistedstate: {
     cookieOptions: {
-      sameSite: 'strict',
+      sameSite: "strict",
     }, // only used when cookie
     // storage: 'localStorage', //'sessionStorage' , 'localStorage
-    debug: true
-    
+    debug: true,
   },
-    
-  css: [
-     '~/assets/styles/css/tailwind.css',
-    '~/assets/styles/scss/main.scss',
-  ],
+
+  css: ["~/assets/styles/css/tailwind.css", "~/assets/styles/scss/main.scss"],
   postcss: {
     plugins: {
       tailwindcss: {},
@@ -69,12 +59,11 @@ export default defineNuxtConfig({
       NUXT_FIREBASE_AUTH_DOMAIN: process.env.NUXT_FIREBASE_AUTH_DOMAIN,
       NUXT_FIREBASE_PROJECT_ID: process.env.NUXT_FIREBASE_PROJECT_ID,
       NUXT_FIREBASE_STORAGE_BUCKET: process.env.NUXT_FIREBASE_STORAGE_BUCKET,
-      NUXT_FIREBASE_MESSAGING_SENDER_ID: process.env.NUXT_FIREBASE_MESSAGING_SENDER_ID,
+      NUXT_FIREBASE_MESSAGING_SENDER_ID:
+        process.env.NUXT_FIREBASE_MESSAGING_SENDER_ID,
       NUXT_FIREBASE_APPID_KEY: process.env.NUXT_FIREBASE_APPID_KEY,
       NUXT_FIREBASE_MEASUREMENT_ID: process.env.NUXT_FIREBASE_MEASUREMENT_ID,
       NUXT_FIREBASE_DATABASE_URL: process.env.NUXT_FIREBASE_DATABASE_URL,
-    }
+    },
   },
-
-
 });
--- pages/certificate/[reference]/[:slug].vue
+++ pages/certificate/[reference]/[:slug].vue
@@ -1,50 +1,65 @@
 <template>
   <button class="btn-print" @click="onPrint()">Print</button>
-  <div v-if="certificate" id="printarea" class="certificate">
-    <div class="header">
-      <div class="certified">
-        <div>
-          <div>Verified</div>
-          <div>Certificate</div>
-          <div>
-            {{
-              certificate.created_at && certificate.created_at.substring(0, 4)
-            }}
+  <div v-if="certificate">
+    <!-- {{ certificate.course && certificate.course.type }} -->
+    <template v-if="certificate.course.type != null">
+      <div class="template">
+        <div class="cert_name">{{ certificate.user.name }}</div>
+        <div class="cert_date">
+          {{ $dayjs(certificate.created_at).format("YYYY-MM-DD") }}
+        </div>
+        <img src="./cert_1.png" />
+      </div>
+    </template>
+    <template v-else>
+      <div id="printarea" class="certificate">
+        <div class="header">
+          <div class="certified">
+            <div>
+              <div>Verified</div>
+              <div>Certificate</div>
+              <div>
+                {{
+                  certificate.created_at &&
+                  certificate.created_at.substring(0, 4)
+                }}
+              </div>
+            </div>
+            <img src="~/assets/images/certified.png" alt="" class="img-fluid" />
+          </div>
+          <div class="banner text-uppercase text-white h1">
+            Certificate of Completion
           </div>
         </div>
-        <img src="~/assets/images/certified.png" alt="" class="img-fluid" />
-      </div>
-      <div class="banner text-uppercase text-white h1">
-        Certificate of Completion
-      </div>
-    </div>
-    <div class="content">
-      <img
-        src="~/assets/images/ingenium-logo.png"
-        alt=""
-        class="ingenum-logo"
-      />
-      <div class="text">This Certificate is awarded to</div>
-      <div class="user-name">{{ certificate.user.name }}</div>
-      <div class="text">For The Successful Completion Of The</div>
-      <div class="course-title">
-        {{ certificate.course.title }}
-      </div>
-      <img src="~/assets/images/qrcode.png" alt="" class="qrcode" />
-      <div class="signature py-3">
-        <img
-          src="~/assets/images/stamp.png"
-          alt="Stamp"
-          class="stamp"
-          height="100"
-        />
-        <div class="text-center">
-          <i class="sign">{{ "Jennifer Orode" }}</i>
-          <hr />
-          <div class="text trainer">{{ "Jennifer Orode" }}</div>
+        <div class="content">
+          <img
+            src="~/assets/images/ingenium-logo.png"
+            alt=""
+            class="ingenum-logo"
+          />
+          <div class="text">This Certificate is awarded to</div>
+          <div class="user-name">{{ certificate.user.name }}</div>
+          <div class="text">For The Successful Completion Of The</div>
+          <div class="course-title">
+            {{ certificate.course.title }}
+          </div>
+          <img src="~/assets/images/qrcode.png" alt="" class="qrcode" />
+          <div class="signature py-3">
+            <img
+              src="~/assets/images/stamp.png"
+              alt="Stamp"
+              class="stamp"
+              height="100"
+            />
+            <div class="text-center">
+              <i class="sign">{{ "Jennifer Orode" }}</i>
+              <hr />
+              <div class="text trainer">{{ "Jennifer Orode" }}</div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </template>
   </div>
 </template>
 
@@ -74,6 +89,11 @@ const onPrint = () => window.print();
   border-radius: 5px;
 }
 
+.template > .cert_name,
+.template > .cert_date {
+  display: none;
+}
+
 @media print {
   /* body * {
     display: none;
@@ -85,10 +105,30 @@ const onPrint = () => window.print();
   .btn-print {
     display: none !important;
   }
+
+  .template > .cert_name,
+  .template > .cert_date {
+    display: inline;
+  }
+
+  .template > .cert_name {
+    width: 100%;
+    position: absolute;
+    font-size: 40px;
+    top: 40%;
+    text-align: center;
+  }
+
+  .template > .cert_date {
+    position: absolute;
+    font-size: large;
+    left: 20%;
+    bottom: 19%;
+  }
 }
 
 @page {
-  size: A4 landscape;
+  /* size: A4 landscape; */
   margin: 0;
 }
 
